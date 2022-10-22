8bc67a70cf56e1bdd625262e999c3e65bb457b9f
sozysozbot <sozysozbot@gmail.com>
2022-10-22 20:41:07 +0900

    Meticulously articulate the victory condition of the game

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 export type Tuple9<T> = [T, T, T, T, T, T, T, T, T];
 export type Hand = ShogiProfession[];
-export type Phase = "piece_phase_completed" | "stone_phase_completed" | "resolved"
-export type GameState = {
-    phase: Phase,
+export type Phase = "piece_phase_played" | "stone_phase_played" | "resolved"
+export type GameState = ResolvedGameState | PiecePhasePlayed | StonePhasePlayed;
+export type GameEnd = {
+    victor: Side | "KarateRPSBoxing",
+    reason: "two_pawns_on_the_same_column" | "king_captured_by_stone" | "king_captured_by_piece" | "king_suicide"
+}
+export type ResolvedGameState = {
+    phase: "resolved",
+    board: Board,
+    hand_of_black: Hand,
+    hand_of_white: Hand,
+    who_goes_next: Side,
+}
+export type PiecePhasePlayed = {
+    phase: "piece_phase_played",
+    board: Board,
+    hand_of_black: Hand,
+    hand_of_white: Hand,
+    who_goes_next: Side,
+}
+export type StonePhasePlayed = {
+    phase: "stone_phase_played",
     board: Board,
     hand_of_black: Hand,
     hand_of_white: Hand,
@@ -41,10 +60,10 @@ type ShogiRowName = "一" | "二" | "三" | "四" | "五" | "六" | "七" | "八
 
 type Coordinate = [ShogiColumnName, ShogiRowName];
 
-type PiecePhaseMove = { 
-    side: Side, 
-    from?: Coordinate, 
-    to: Coordinate, 
+type PiecePhaseMove = {
+    side: Side,
+    from?: Coordinate,
+    to: Coordinate,
     prof: ShogiProfession | ChessProfession | KingProfession,
     promotes?: boolean // true → 成. false → 不成. not given → cannot promote
 }
@@ -85,7 +104,7 @@ const get_initial_state: (who_goes_first: Side) => GameState = (who_goes_first: 
                 { type: "ス", side: "白", prof: "ポ", can_castle: false },
                 { type: "ス", side: "白", prof: "ポ", can_castle: false },
                 { type: "ス", side: "白", prof: "ポ", can_castle: false },
-                { type: "ス", side: "白", prof: "ポ", can_castle: false }, 
+                { type: "ス", side: "白", prof: "ポ", can_castle: false },
                 { type: "ス", side: "白", prof: "ポ", can_castle: false },
                 { type: "ス", side: "白", prof: "ポ", can_castle: false },
                 { type: "ス", side: "白", prof: "ポ", can_castle: false },
@@ -128,4 +147,57 @@ const get_initial_state: (who_goes_first: Side) => GameState = (who_goes_first: 
             ],
         ]
     }
+}
+
+function apply_piece_phase_move(old: ResolvedGameState, piece_phase: PiecePhaseMove): PiecePhasePlayed {
+    throw new Error("未実装");
+}
+function place_stone(old: PiecePhasePlayed, side: Side, stone_to: Coordinate): StonePhasePlayed {
+    throw new Error("未実装");
+}
+
+function invertSide(side: Side): Side {
+    if (side === "黒") return "白";
+    else return "黒";
+}
+
+/** 石フェーズが終了した後、勝敗判定と囲碁検査をする。 / To be called after a stone is placed: checks the victory condition and the game-of-go condition.
+ * 
+ * 1. キング王が盤面から除かれているかを判定。除かれていたら、それは「駒による勝利」
+ * 2. 囲碁の判定をする。以下 2-1. と 2-2. の順番を逆にするとコンビネーションアタックが失敗する
+ *   2-1. 自分の駒と石によって囲まれている相手の駒と石をすべて取り除く
+ *   2-2. 相手の駒と石によって囲まれている自分の駒と石をすべて取り除く
+ * 3. 二ポが発生しているかを判断。発生していた場合、反則負け
+ * 4. キング王が盤面から除かれているかを判定。
+ *   4-1. 両者が除かれていたら、@re_hako_moon曰く引き分けとなり、カラテジャンケンボクシング
+ *   4-2. 相手の王だけ除かれていたら、それは「石による勝利」
+ *   4-3. 自分の王だけ除かれていたら、それは「王の自殺による敗北」
+ *   4-4. 「ごっそり」（@re_hako_moon曰く、2個か3個）に該当するときには「ショゴス！」の発声
+ * 
+ * 1. Checks whether a king is removed from the board. If removed, then it is a victory by a piece.
+ * 2. Checks the conditions pertaining to the game of go. (Note that the order of 2-1. and 2-2. is crucial; otherwise the "combination attack" cannot happen)
+ *   2-1. Remove all the opponent's pieces and stones surrounded by your pieces and stones
+ *   2-2. Remove all your pieces and stones surrounded by the opponent's pieces and stones
+ * 3. Checks whether two pawns occupy the same column. If so, the player loses.
+ * 4. Checks whether a king is removed from the board.
+ *   4-1. If both kings are removed, that is a draw (according to @re_hako_moon), and therefore a Karate Rock-Paper-Scissors Boxing
+ *   4-2. If the opponent's king is removed but yours remains, then it's a victory by stones.
+ *   4-3. If your king is removed but the opponent's remains, then it's a king's suicide.
+ *   4-4. When a large number (>= 2 or 3, according to @re_hako_moon) is removed, then "ShoGoSs!" should be shouted
+ **/
+function resolve(after_stone_phase: StonePhasePlayed): ResolvedGameState | GameEnd {
+    throw new Error("未実装");
+}
+
+function from_resolved_to_resolved(old: ResolvedGameState, move: Move): ResolvedGameState | GameEnd {
+    const after_piece_phase = apply_piece_phase_move(old, move.piece_phase);
+
+    const after_stone_phase: StonePhasePlayed = move.stone_to ? place_stone(after_piece_phase, move.piece_phase.side, move.stone_to) : {
+        phase: "stone_phase_played",
+        board: after_piece_phase.board,
+        hand_of_black: after_piece_phase.hand_of_black,
+        hand_of_white: after_piece_phase.hand_of_white,
+        who_goes_next: invertSide(after_piece_phase.who_goes_next),
+    };
+    return resolve(after_stone_phase)
 }
