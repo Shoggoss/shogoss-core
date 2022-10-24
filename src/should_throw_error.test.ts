47e7fb6e5b344b1a32aa322f9f15a7a73e736728
sozysozbot / hsjoihs <sozysozbot@gmail.com>
2022-10-24 21:44:25 +0900

    Add the test cases for castling

--- src/should_throw_error.test.ts
+++ src/should_throw_error.test.ts
@@ -1,6 +1,89 @@
 import { from_custom_state, get_initial_state, main } from ".";
 import { put_entity_at_coord_and_also_adjust_flags } from "./board";
 
+test('castling failure by piece', () => {
+	expect(() => main([
+		{ piece_phase: { side: "黒", to: ["４", "六"], prof: "ポ" } },
+		{ piece_phase: { side: "白", to: ["４", "四"], prof: "ポ" } },
+		{ piece_phase: { side: "黒", to: ["３", "六"], prof: "ナ" } },
+		{ piece_phase: { side: "白", to: ["４", "二"], prof: "キ" } },
+		{ piece_phase: { side: "黒", to: ["６", "八"], prof: "キ" } },
+		{ piece_phase: { side: "白", to: ["２", "二"], prof: "キ" } }, // castle
+	])).toThrowError("白が４二から２二へとキング王をキャスリングしようとしていますが、キング王とルークの間に駒があるのでキャスリングできません")
+});
+
+test('castling failure by rook not being there', () => {
+	expect(() => main([
+		{ piece_phase: { side: "黒", to: ["４", "六"], prof: "ポ" } },
+		{ piece_phase: { side: "白", to: ["４", "四"], prof: "ポ" } },
+		{ piece_phase: { side: "黒", to: ["５", "六"], prof: "ビ" } },
+		{ piece_phase: { side: "白", to: ["５", "四"], prof: "ビ" } },
+		{ piece_phase: { side: "黒", to: ["３", "六"], prof: "ナ" } },
+		{ piece_phase: { side: "白", to: ["３", "四"], prof: "ナ" } },
+		{ piece_phase: { side: "黒", to: ["４", "七"], prof: "ク" } },
+		{ piece_phase: { side: "白", to: ["４", "二"], prof: "キ" } },
+        { piece_phase: { side: "黒", to: ["２", "八"], prof: "ル" } },
+		{ piece_phase: { side: "白", to: ["２", "二"], prof: "ル" } },
+		{ piece_phase: { side: "黒", to: ["６", "八"], prof: "キ" } },
+		{ piece_phase: { side: "白", to: ["２", "二"], prof: "キ" } }, // castle
+	])).toThrowError("白が４二から２二へとキング王をキャスリングしようとしていますが、１二にルークがないのでキャスリングできません");
+});
+
+test('castling failure by rook having moved', () => {
+	expect(() => main([
+		{ piece_phase: { side: "黒", to: ["４", "六"], prof: "ポ" } },
+		{ piece_phase: { side: "白", to: ["４", "四"], prof: "ポ" } },
+		{ piece_phase: { side: "黒", to: ["５", "六"], prof: "ビ" } },
+		{ piece_phase: { side: "白", to: ["５", "四"], prof: "ビ" } },
+		{ piece_phase: { side: "黒", to: ["３", "六"], prof: "ナ" } },
+		{ piece_phase: { side: "白", to: ["３", "四"], prof: "ナ" } },
+		{ piece_phase: { side: "黒", to: ["４", "七"], prof: "ク" } },
+		{ piece_phase: { side: "白", to: ["４", "二"], prof: "キ" } },
+        { piece_phase: { side: "黒", to: ["２", "八"], prof: "ル" } },
+		{ piece_phase: { side: "白", to: ["２", "二"], prof: "ル" } },
+        { piece_phase: { side: "黒", to: ["１", "八"], prof: "ル" } },
+		{ piece_phase: { side: "白", to: ["１", "二"], prof: "ル" } },
+		{ piece_phase: { side: "黒", to: ["６", "八"], prof: "キ" } },
+		{ piece_phase: { side: "白", to: ["２", "二"], prof: "キ" } }, // castle
+	])).toThrowError("白が４二から２二へとキング王をキャスリングしようとしていますが、１二にあるルークは既に動いたことがあるルークなのでキャスリングできません");
+});
+
+test('castling failure by being in check', () => {
+	expect(() => main([
+		{ piece_phase: { side: "黒", to: ["４", "五"], prof: "ポ" } },
+		{ piece_phase: { side: "白", to: ["４", "四"], prof: "ポ" } },
+		{ piece_phase: { side: "黒", to: ["３", "六"], prof: "ナ" } },
+		{ piece_phase: { side: "白", to: ["３", "四"], prof: "ク" } },
+		{ piece_phase: { side: "黒", to: ["４", "八"], prof: "キ" } },
+		{ piece_phase: { side: "白", to: ["４", "五"], prof: "ク" } }, 
+		{ piece_phase: { side: "黒", to: ["２", "八"], prof: "キ" } }, // castle
+	])).toThrowError("黒が４八から２八へとキング王をキャスリングしようとしていますが、相手からの王手（チェック）が掛かっているのでキャスリングできません")
+});
+
+test('castling failure by enemy seeing the square that the king moves through', () => {
+	expect(() => main([
+		{ piece_phase: { side: "黒", to: ["４", "六"], prof: "ポ" } },
+		{ piece_phase: { side: "白", to: ["３", "四"], prof: "ナ" } },
+		{ piece_phase: { side: "黒", to: ["３", "六"], prof: "ナ" } },
+		{ piece_phase: { side: "白", to: ["２", "六"], prof: "ナ" } },
+		{ piece_phase: { side: "黒", to: ["４", "八"], prof: "キ" } },
+		{ piece_phase: { side: "白", to: ["７", "四"], prof: "ナ" } }, 
+		{ piece_phase: { side: "黒", to: ["２", "八"], prof: "キ" } }, // castle
+	])).toThrowError("黒が４八から２八へとキング王をキャスリングしようとしていますが、通過点のマスに敵の駒の利きがあるのでキャスリングできません")
+});
+
+test('castling failure by enemy seeing the square that the king ends up in', () => {
+	expect(() => main([
+		{ piece_phase: { side: "黒", to: ["４", "五"], prof: "ポ" } },
+		{ piece_phase: { side: "白", to: ["７", "四"], prof: "ナ" } },
+		{ piece_phase: { side: "黒", to: ["３", "六"], prof: "ナ" } },
+		{ piece_phase: { side: "白", to: ["６", "六"], prof: "ナ" } },
+		{ piece_phase: { side: "黒", to: ["４", "八"], prof: "キ" } },
+		{ piece_phase: { side: "白", to: ["４", "七"], prof: "ナ" } }, 
+		{ piece_phase: { side: "黒", to: ["２", "八"], prof: "キ" } }, // castle
+	])).toThrowError("黒が４八から２八へとキング王をキャスリングしようとしていますが、移動先のマスに敵の駒の利きがあるのでキャスリングできません")
+});
+
 test('en passant fails', () => {
     const state = get_initial_state("黒");
 
